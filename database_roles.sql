a355b1dd5e802013eac7bfeef39233d9405208d8
cat-plevak <catherine.plevak@colorado.edu>
2017-09-19 07:58:32 -0600

    part 1 done

---
+++ database_roles.sql
@@ -0,0 +1,33 @@
+createdb galvanize_contacts;
+CREATE ROLE galvanize_admin WITH LOGIN PASSWORD 'galvanize_P@$$w0rd';
+ALTER ROLE galvanize_admin WITH LOGIN;
+REVOKE ALL PRIVILEGES ON galvanize_contacts;
+GRANT ALL PRIVILEGES ON DATABASE galvanize_contacts TO public;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL tables IN SCHEMA public TO galvanize_admin;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO galvanize_admin;
+
+
+--
+-- PostgreSQL database cluster dump
+--
+
+SET default_transaction_read_only = off;
+
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+
+--
+-- Roles
+--
+
+CREATE ROLE catherineplevak;
+ALTER ROLE catherineplevak WITH SUPERUSER INHERIT CREATEROLE CREATEDB LOGIN REPLICATION BYPASSRLS;
+CREATE ROLE galvanize_admin;
+ALTER ROLE galvanize_admin WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD 'galvanize_P@$$w0rd';
+
+
+
+
+--
+-- PostgreSQL database cluster dump complete
+--
